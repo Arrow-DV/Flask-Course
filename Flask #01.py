1bd6e35efe273b6b8fcd4eec077bed8c5520b535
Arrow <128916762+Arrow-DV@users.noreply.github.com>
2024-05-27 15:10:06 +0300

    Update Flask #01.py

--- Flask #01.py
+++ Flask #01.py
@@ -1,1 +1,17 @@
+# Import Flask Module
+from flask import Flask 
+# Init The Main App
+app = Flask(__name__)
+# Set App Routes
+# @variable_name.route('address')
+@app.route('/')
+@app.route('/home')
+def index():
+    return '<h1>This Is HomePage</h1>'
 
+@app.route('/info')
+@app.route('/about')
+def about():
+    return 'This Is About Page'
+# Run The Server With Debug Mode
+app.run(debug=True)
